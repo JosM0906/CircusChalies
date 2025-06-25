45ada8f6bf8162933cfe4f418691579ce2ea90e5
Diana Avendano <digua2021@icloud.com>
2025-06-25 00:34:07 -0600

    comentarios y updates de stage02

--- src/js/states/Stage02.js
+++ src/js/states/Stage02.js
@@ -21,28 +21,27 @@ GameCtrl.Stage02 = function () {
     this.physics;        //        the physics manager
     this.rnd;                //        the repeatable random number generator
 */
-    //        You can use any of these from any function within this State.
-    //        But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
-
+    
 };
 
 GameCtrl.Stage02.prototype = {
     
     /**
-     * Draw the distance
+     * Dibuja los metros para que el jugador vea qué tanto ha avanzado.
      */
     _createMeters:function(){
-        var graphics = this.game.add.graphics(0, 0);
+        var graphics = this.game.add.graphics(0, 0); // Crea un objeto para dibujar
         var x;
         for(var i=10;i>=0;i--){
             x=(10-i)*705;
+            // Escribimos el número de metros
             this.add.text(x+15, 694, (i*10)+' m', {
                 font : '46px "arcadeclasic"',
                 fill : '#fff',
                 align : 'left'
             });
 
-            
+             // Dibujamos rectángulos como referencia en el suelo
             graphics.lineStyle(2, 0x000000, 1);
             graphics.beginFill(0x000000, 1);
             graphics.drawRect(x, 690, 130, 50);
@@ -54,11 +53,11 @@ GameCtrl.Stage02.prototype = {
 
     },
     /**
-     * Create player group (the clown and the lion)
+     * Crea el jugador (el payaso que camina en la cuerda floja)
      */
     _createPlayer:function(){
-        this.player=this.add.sprite(85, 348, 'clown','walkBalance0');
-        this.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.player=this.add.sprite(85, 348, 'clown','walkBalance0'); // Coloca el sprite del jugador
+        this.physics.enable(this.player, Phaser.Physics.ARCADE); // Activar físicas para colisiones y gravedad
         
         this.player.scale.x =3;
         this.player.scale.y =3;
@@ -66,102 +65,111 @@ GameCtrl.Stage02.prototype = {
         this.physics.enable(this.player, Phaser.Physics.ARCADE,true);
         
                 
-        this.player.body.setSize(35, 69, 0, 0);
+        this.player.body.setSize(35, 69, 0, 0); // Área de colisión del jugador
         
 
-        this.player.animations.add('walkBalance', Phaser.Animation.generateFrameNames('walkBalance', 0, 2, '', 0), 3 /*fps */, true);
+        this.player.animations.add('walkBalance', Phaser.Animation.generateFrameNames('walkBalance', 0, 2, '', 0), 3 /*fps */, true); // Animación al caminar
 
-        this.player.body.collideWorldBounds=true;
+        this.player.body.collideWorldBounds=true; // No salirse del mapa
         
     },
+    /**
+     * Quita monos que ya salieron de pantalla.
+     */
     _monkyOut:function(m){
-        this.monkeys.remove(m);
+        this.monkeys.remove(m); // Removemos el mono que salió
     },
-        
+    /**
+     * Crea un mono que aparecerá como obstáculo.
+     */
     _addMonkey:function(){
         if(this.monkeys.total>3){
-            return;
+            return; // No más de 3 monos a la vez
         }
-
+    
         var createMonky=function(x){
+            // Calcula dónde colocar al nuevo mono
             if(!x){
-                x=this.player.x +950+this.rnd.integerInRange(-400,200);
+                x=this.player.x +950+this.rnd.integerInRange(-400,200); // Una posición aleatoria delante del jugador
                 var xLast=(this.monkeys.length>0) ? this.monkeys.getAt(this.monkeys.length-1).x : false;
                 x=(xLast && (x-xLast)<500) ? xLast+500 : x;
             }
             
 
-            var monkey=this.monkeys.create(x, 372, 'clown','monkey0');
+            var monkey=this.monkeys.create(x, 372, 'clown','monkey0'); // Añadimos el sprite del mono
             monkey.scale.x=3;
             monkey.scale.y=3;
-            monkey.body.velocity.x=-90;
-            monkey.animations.add('monkey', Phaser.Animation.generateFrameNames('monkey', 0, 2, '', 0), 3 /*fps */, true);
-            monkey.animations.play('monkey',6);
+            monkey.body.velocity.x=-90; // Los monos se mueven hacia la izquierda
+            monkey.animations.add('monkey', Phaser.Animation.generateFrameNames('monkey', 0, 2, '', 0), 3 /*fps */, true); // Animación del mono
+            monkey.animations.play('monkey',6);  // Lo animamos
             monkey.checkWorldBounds = true;
-            monkey.events.onOutOfBounds.add(this._monkyOut, this);
-            monkey.body.setSize(44, 49, 0, 0);
-            monkey.body.gravity.y=400;
+            monkey.events.onOutOfBounds.add(this._monkyOut, this); // Cuando se salga de pantalla, se remueve
+            monkey.body.setSize(44, 49, 0, 0); // Área de colisión
+            monkey.body.gravity.y=400; // Gravedad para que caiga
             return monkey;
         };
 
-        var difficulty=this.rnd.integerInRange(1,100);
-        var nMonkeys=(difficulty>40)? this.rnd.integerInRange(1,4) : 1;
+        var difficulty=this.rnd.integerInRange(1,100); // Calculamos dificultad
+        var nMonkeys=(difficulty>40)? this.rnd.integerInRange(1,4) : 1; // Cuántos monos aparecen
 
         var distance=50;
         
-        var jumpTime=this.rnd.integerInRange(200,700);
+        var jumpTime=this.rnd.integerInRange(200,700); // Cada cuánto saltan
 
         for(var i=0;i<nMonkeys;i++){
             difficulty=100;
             if(i>1){
                 distance=100*i;
-                jumpTime=this.rnd.integerInRange(1000,2000);
+                jumpTime=this.rnd.integerInRange(1000,2000); // Más tiempo entre saltos
             }
 
             var x=(i>0) ? this.monkeys.getAt(this.monkeys.length-1).x+distance : null;
-            var _monkey=createMonky.call(this,x);
+            var _monkey=createMonky.call(this,x); // Crear mono
             
             _monkey.jumpTime=(difficulty>70)? jumpTime:false;
             _monkey.lastJump=this.time.lastTime;
         }
     },
     /**
-     * Create the static obstacles (firepots)
+     * Crea el grupo que contiene a los monos (se llama cuando arranca el nivel).
      */
     _createObstacles:function(){
         this.monkeys=this.add.group();
         this.monkeys.enableBody = true;
         this.monkeys.physicsBodyType = Phaser.Physics.ARCADE;
         
-        this.game.time.events.loop(Phaser.Timer.SECOND, this._addMonkey, this);
+        this.game.time.events.loop(Phaser.Timer.SECOND, this._addMonkey, this); // Cada segundo, intentamos poner monos nuevos
 
     },
+    /**
+     * Esta es la función que arranca cuando empieza el nivel.
+     */
     create: function () {
        
         this.gameover=false;
-        this.music = this.add.audio('stage2');
+        this.music = this.add.audio('stage2'); // Sonido de fondo
         this.music.play();
         
 
-        this.cursors =this.game.input.keyboard.createCursorKeys();
-        this.world.setBounds(0,0,1024 * 8, 200);
+        this.cursors =this.game.input.keyboard.createCursorKeys(); // Controles con flechas
+        this.world.setBounds(0,0,1024 * 8, 200); // Ancho del mundo
         //this.background=this.game.add.tileSprite(0, 200, 1024, 552, 'background');
         this.background=this.add.tileSprite(0, 200, 1024 * 8, 552, 'stage02');
 
 
 
-        this._createMeters();
-        this._createPlayer();
-        this._createObstacles();
+        this._createMeters(); // Dibujamos metros
+        this._createPlayer(); // Creamos jugador
+        this._createObstacles(); // Creamos obstáculos
         
 
-        this.floor = this.game.add.sprite(0, 417);
-        this.endStage=this.game.add.sprite(1024*8-300, 620, 'clown','endLevel1');
+        this.floor = this.game.add.sprite(0, 417); // Piso invisible
+        this.endStage=this.game.add.sprite(1024*8-300, 620, 'clown','endLevel1'); // Meta del nivel
         this.physics.enable(this.floor, Phaser.Physics.ARCADE);
         this.physics.enable(this.endStage, Phaser.Physics.ARCADE);
         this.endStage.scale.x=3;
         this.endStage.scale.y=3;
-        this.endStage.body.immovable = true;
+        this.endStage.body.immovable = true; // Para que no se mueva la meta
 
         //this.endStage.body.checkCollision.left = false;
         //this.endStage.body.checkCollision.right = false;
@@ -172,20 +180,25 @@ GameCtrl.Stage02.prototype = {
         this.floor.body.collideWorldBounds = true;
         this.floor.body.width = this.game.world.width;
 
-        this.recicleFireCirclesWall = this.game.add.sprite(-12, 600);
+        this.recicleFireCirclesWall = this.game.add.sprite(-12, 600); // Una pared invisible para reciclar fuego (no se usa en este nivel)
         this.physics.enable(this.recicleFireCirclesWall, Phaser.Physics.ARCADE);
         this.recicleFireCirclesWall.body.immovable = true;
         this.recicleFireCirclesWall.body.height = 500;
         this.recicleFireCirclesWall.body.width = 2;
          
     },
+
+    /**
+     * Esta función sucede cuando pierdes.
+     */
     triggerGameover: function(){
         var that=this;
         this.music.stop();
-        this.failureSound=this.add.audio('failure');
+        this.failureSound=this.add.audio('failure'); // Sonido al perder
         this.failureSound.play();
          
         setTimeout(function(){
+            // Detenemos animación y movimiento del jugador
             that.player.animations.stop();
             that.player.frameName='clownburn0000';
 
@@ -194,6 +207,7 @@ GameCtrl.Stage02.prototype = {
             that.player.body.velocity.y=0;
             that.player.body.velocity.x=0;
 
+            // Detenemos a los monos
             that.monkeys.setAll('body.velocity.x',0);
             that.monkeys.setAll('body.gravity.y',0);
             that.monkeys.setAll('body.velocity.y',0);
@@ -202,15 +216,19 @@ GameCtrl.Stage02.prototype = {
         },1);
         
         setTimeout(function(){
-            that.game.state.start('Stage02');
+            that.game.state.start('Stage02'); // Reinicia el nivel después de un tiempo
             that.failureSound.stop();
         },3100);
 
         this.gameover=true;
     },
+
+    /**
+     * Esta función sucede muchas veces por segundo para mover y actualizar todo.
+     */
     update: function () {
         if(this.gameover){
-            return;
+            return; // Si ya perdiste, no hace nada
         }
 
     /*    if(this.player.body.x<(this.world.width-1600)){
@@ -223,22 +241,24 @@ GameCtrl.Stage02.prototype = {
 
         
       */
+      // Revisamos colisión entre el jugador y los monos
         this.game.physics.arcade.collide(this.player, this.monkeys, this.triggerGameover, null, this);
-
+        // Hacemos que los monos salten si tienen jumpTime
         this.monkeys.forEach(function(m){
             if(m.jumpTime && m.body.y<370 && ((m.lastJump-this.time.lastTime)<m.jumpTime)){
-                m.body.velocity.y=-500;
+                m.body.velocity.y=-500; // Saltar
             }
         }, this);
 
-        this.game.physics.arcade.collide(this.endStage, this.player);
-        this.game.physics.arcade.collide(this.floor, this.player);
-        this.game.physics.arcade.collide(this.floor, this.monkeys);
+        this.game.physics.arcade.collide(this.endStage, this.player); // Llegar a la meta
+        this.game.physics.arcade.collide(this.floor, this.player); // Caminar en el piso
+        this.game.physics.arcade.collide(this.floor, this.monkeys); // Los monos caminan sobre el piso también
 
-        this.player.body.gravity.y=800;
+        this.player.body.gravity.y=800; // Gravedad para que caiga
 
         var isJumping=!this.player.body.touching.down;
-        this.game.camera.x=this.player.x-120;
+        this.game.camera.x=this.player.x-120; // La cámara sigue al jugador
+       // Cambiamos el sprite según si salta o camina
         if(isJumping){
             this.player.frameName='jumpBalance';
         }
@@ -247,32 +267,36 @@ GameCtrl.Stage02.prototype = {
             this.player.body.velocity.y=-480;
         }
         
-
+        
         if(isJumping){
             // Mantengo la velocidad del fondo
 
-            return;
+            return; // Cuando está saltando, solo cae
         }
 
         if (this.cursors.right.isDown){
             
-            this.player.body.velocity.x=150;
+            this.player.body.velocity.x=150; // Avanzar
             
-            this.player.animations.play('walkBalance', 10, true);
+            this.player.animations.play('walkBalance', 10, true); // Animación de caminar
         }else if (this.cursors.left.isDown){
             
-            this.player.body.velocity.x=-130;
+            this.player.body.velocity.x=-130; // Ir a la izquierda
             this.player.animations.play('walkBalance', 5, true);
         }else{
-            this.player.body.velocity.x=0;
+            this.player.body.velocity.x=0; // Quieto
                 
             this.player.animations.stop();
-            this.player.frameName='walkBalance2';
+            this.player.frameName='walkBalance2'; // Imagen estática
             //this.player.animations.play('walkBalance',8,true);
         }
 
 
     },
+
+    /**
+     * Esta función es solo para dibujar las cajas invisibles cuando CIRCUSDEBUG es true.
+     */
     render: function(){
         /* global CIRCUSDEBUG */
         //CIRCUSDEBUG=true;
@@ -294,6 +318,10 @@ GameCtrl.Stage02.prototype = {
                 this.game.debug.renderPhysicsBody(e.body);
         }, this);*/
     },
+
+    /**
+     * Esta función es para salir del juego y volver al menú.
+     */
     quitGame: function () {
 
             //        Here you should destroy anything you no longer need.
