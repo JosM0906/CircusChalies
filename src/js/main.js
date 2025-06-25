43739e7876832d85e4c887be5e8218da4c7a421f
Diana Avendano <digua2021@icloud.com>
2025-06-25 00:47:40 -0600

    Prueba

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,23 @@
 'use strict';
 /* global GameCtrl */
-
-// Este es el inicio del juego.
-// Estamos creando un juego de Phaser y metiéndolo en un div que tenga el id "game".
-// Usamos un tamaño de pantalla de 1024 x 768, que es ancho x alto.
-var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'game', false, false);
+//        Create your Phaser game and inject it into the game div.
+//        We did it in a window.onload event, but you can do it anywhere (requireJS load, anonymous function, jQuery dom ready, - whatever floats your boat)
+//        We're using a game size of 1024 x 768 here, but you can use whatever you feel makes sense for your game of course.
+var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'game',false,false);
 
 /* exported CIRCUSDEBUG */
-// Esta variable es para depuración, por ahora es falsa.
-// Si luego la pones en true podrás ver cosas útiles para programar.
-var CIRCUSDEBUG = false;
+var CIRCUSDEBUG=false;
 
-// Aquí añadimos los "estados" del juego, es decir, las pantallas o escenas que va a tener.
-// Por ejemplo: Boot es cuando arranca el juego, Preloader es cuando carga recursos, etc.
-game.state.add('Boot', GameCtrl.Boot);         // Estado inicial para preparar el juego
-game.state.add('Preloader', GameCtrl.Preloader); // Estado que carga imágenes, sonidos, etc.
-game.state.add('MainMenu', GameCtrl.MainMenu);  // Menú principal del juego
-game.state.add('Prestage', GameCtrl.Prestage);  // Una escena entre niveles
-game.state.add('Stage01', GameCtrl.Stage01);    // Primer nivel
-game.state.add('Stage02', GameCtrl.Stage02);    // Segundo nivel
 
-// Por último arrancamos el juego desde el estado 'Boot' para que todo empiece.
+	//        Add the States your game has.
+	//        You don't have to do this in the html, it could be done in your Boot state too, but for simplicity I'll keep it here.
+game.state.add('Boot', GameCtrl.Boot);
+game.state.add('Preloader', GameCtrl.Preloader);
+game.state.add('MainMenu', GameCtrl.MainMenu);
+game.state.add('Prestage', GameCtrl.Prestage);
+game.state.add('Stage01', GameCtrl.Stage01);
+game.state.add('Stage02', GameCtrl.Stage02);
+
+	//        Now start the Boot state.
 game.state.start('Boot');
 
